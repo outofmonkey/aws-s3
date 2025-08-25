b27693b7e008186af7208f37ab36084b2ebab0e7
outofmonkey <128472571+outofmonkey@users.noreply.github.com>
2025-08-25 15:34:15 +0530

    Add files via upload

---
+++ s3_bucket.tf
@@ -0,0 +1,26 @@
+
+resource "aws_s3_bucket" "mys3bucket" {
+  bucket = "mybuckets3bucket100"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "test"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "version" {
+  bucket = aws_s3_bucket.mys3bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_object" "object" {
+    depends_on = [aws_s3_bucket.mys3bucket, aws_s3_bucket_versioning.version]
+  bucket = aws_s3_bucket.mys3bucket.id
+  key    = "install-web.sh"
+  source = "${path.module}/install-web.sh"
+  etag = filemd5("${path.module}/install-web.sh")
+    storage_class = "STANDARD"
+}
